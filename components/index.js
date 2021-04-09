eb692e7663d472fcf34179c89359aad83817b539
Omar <omaar_5@hotmail.com>
2021-04-09 15:12:32 +0200

    HeaderOption Component

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as Avatar } from "./Avatar";
 export { default as Footer } from "./Footer";
 export { default as SearchHeader } from "./SearchHeader";
 export { default as HeaderOptions } from "./HeaderOptions";
+export { default as HeaderOption } from "./HeaderOption";
