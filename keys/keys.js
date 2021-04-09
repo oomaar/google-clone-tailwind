2ed36ebe43184738c6770cd06ca61e9f18cb65cb
Omar <omaar_5@hotmail.com>
2021-04-09 19:10:13 +0200

    refs

---
+++ keys/keys.js
@@ -0,0 +1,8 @@
+// API Key - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = ''
+
+// Context Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = ''
+
+// <script async src="https://cse.google.com/cse.js?cx="></script>
+// <div class="gcse-search"></div>
