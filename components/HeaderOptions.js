ec837f2ee23a09572275d517e42e48e0a0d16e03
Omar <omaar_5@hotmail.com>
2021-04-09 15:25:59 +0200

    headerOptions Component

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,7 +1,28 @@
+import { HeaderOption } from ".";
+import {
+    SearchIcon,
+    PhotographIcon,
+    PlayIcon,
+    NewspaperIcon,
+    MapIcon,
+    DotsVerticalIcon,
+} from "@heroicons/react/outline";
+
 const HeaderOptions = () => {
     return (
-        <div>
-            
+        <div className="headerOptions__Container">
+            <div className="flex space-x-3 sm:space-x-6">
+                <HeaderOption Icon={SearchIcon} title="All" selected />
+                <HeaderOption Icon={PhotographIcon} title="Images" />
+                <HeaderOption Icon={PlayIcon} title="Videos" />
+                <HeaderOption Icon={NewspaperIcon} title="News" />
+                <HeaderOption Icon={MapIcon} title="Maps" />
+                <HeaderOption Icon={DotsVerticalIcon} title="More" />
+            </div>
+            <div className="flex space-x-4">
+                <p className="link">Settings</p>
+                <p className="link">Tools</p>
+            </div>
         </div>
     );
 };
